b095cefc980ddcf54810d99dbc25b36e4d622d36
Robin van Leur <robinvanleur777@gmail.com>
2024-01-14 16:38:56 +0100

    algemene functies en reclame toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,9 @@
+def mijn_functie_1(a):
+    return a**2
+
+def mijn_functie_2(a,b):
+    som = int(a+b)
+    deductie = int(a-b)
+    vermenigvuldiging = int(a*b)
+    delen = int(a/b)
+    return [som, deductie, vermenigvuldiging, delen]
---
+++ reclame.py
@@ -0,0 +1,30 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak,prijs,korting):
+    tekst = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs - prijs * korting} euro."
+    return tekst
+
+def inkomsten_totaal(inkomsten):
+    return sum(inkomsten)
+
+def inkomsten_totaal(inkomsten,btw):
+    tekst = f"Het totaal van alle inkomsten van deze week is {sum(inkomsten)} euro, waarover {sum(inkomsten) * btw} euro btw betaald dient te worden."
+    return tekst
+
+def laag_en_hoog(mijn_lijst):
+    return [max(mijn_lijst), min(mijn_lijst)]
+
+def gemiddelde(mijn_lijst):
+    return sum(mijn_lijst) / len(mijn_lijst)
+
+def gemiddelde(mijn_lijst):
+    tekst = f"De gemiddelde inkomsten deze week zijn {sum(mijn_lijst) / len(mijn_lijst)} euro"
+    return tekst
+
+def meervoudig(invoer_lijst):
+    return laag_en_hoog(invoer_lijst)
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+    return uitvoer
