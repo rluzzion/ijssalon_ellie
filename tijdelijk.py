56126129dbefff7357b842b9297f11de7c56357b
Robin van Leur <robinvanleur777@gmail.com>
2024-01-14 09:06:04 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,15 @@
+prijzen = {
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5",
+}
+aanbieding = float(prijzen["aardbei"]) * 0.8
+reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}")
+reclame_tekst2 = reclame_tekst[:62]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    else:
+        print(el.lower())
