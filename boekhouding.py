d689d2b4ec43da5c84f20839c865969497097d5f
Robin van Leur <robinvanleur777@gmail.com>
2024-01-22 13:07:26 +0100

    Huiswerk Les 10 Afgerond

--- boekhouding.py
+++ boekhouding.py
@@ -1,3 +1,5 @@
+import csv
+
 from presentatie import *
 
 from helper import *
@@ -10,5 +12,9 @@ inkomsten = {
 }
 
 totaal_inkomsten = som(inkomsten)
-print(totaal_inkomsten)
 presenteer(inkomsten,totaal_inkomsten)
+
+with open('boekhouding.csv', 'w', newline=") as csvfile:
+          for key, value in inkomsten.items():
+          writer = csv.writer(csvfile, delimiter=';')
+          writer.writerow([key,value])
--- presentatie.py
+++ presentatie.py
@@ -1,5 +1,7 @@
 def presenteer(a,totaal):
-    for k,v in a:
-        print(k,v)
+    print(f"Aardbeien-ijs-totaal : ", a["Aardbeien-ijs-totaal"], "euro")
+    print(f"Vanille-ijs-totaal : ", a["Vanille-ijs-totaal"], "euro")
+    print(f"Chocolade-ijs-totaal : ", a["Chocolade-ijs-totaal"], "euro")
+    print(f"Waterijsjes-totaal : ", a["Waterijsjes-totaal"], "euro")
     print("==========================")
-    print("totaal:", totaal)
+    print("Totaal:", totaal, "euro")
